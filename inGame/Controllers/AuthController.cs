74706c0fa3908407f5ea071ebe9531d8fe87f6eb
Arif <karazhayev@internet.ru>
2023-03-10 10:34:55 +0300

    Добавлен AuthController

---
+++ inGame/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using inGame.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace inGame.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("SignUp")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            if (request == null)
+                return BadRequest(request);
+            return Ok();
+        }
+        [HttpPost("SignIn")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            return Ok();
+        }
+
+        // Методы
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computeHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
